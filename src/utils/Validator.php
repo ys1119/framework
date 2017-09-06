111690f7a463a92639f28bb7c1094323d5a3410d
yangsheng <365046822@qq.com>
2017-09-06 10:46:50 +0800

    validator

--- src/utils/Validator.php
+++ src/utils/Validator.php
@@ -10,38 +10,58 @@ class Validator
 {
     public $success = true;
     public $errors = [];
-    private $reasons = [];
+
     private $data;
     private $rules;
+    private $reasons = [
+        'required' => 'The :attribute field is required.',
+        'required_if' => 'The :attribute field is required.',
+        'email' => 'The :attribute format is invalid.',
+        'numeric' => 'The :attribute field must be number.',
+        'integer' => 'The :attribute field must be integer.',
+        'string' => 'The :attribute field must be string.',
+        'array' => 'The :attribute field must be array.',
+        'json' => 'The :attribute field must be json.',
+        'url' => 'The :attribute field must be url.',
+        'date' => 'The :attribute field must be date.',
+        'ip' => 'The :attribute field must be ip.',
+        'ip4' => 'The :attribute field must be ip4.',
+        'ip6' => 'The :attribute field must be ip6.',
+        'alpha' => 'The :attribute format is invalid.',
+        'alpha_dash' => 'The :attribute format is invalid.',
+        'alpha_num' => 'The :attribute format is invalid.',
+        'min' => 'The :attribute must be at least :min.',
+        'max' => 'The :attribute may not be greater than :max.',
+        'length' => 'The :attribute field out of length :length.',
+        'range' => 'The :attribute field out of range :range.',
+    ];
     public function __construct($data, $rules)
     {
         $this->data = $data;
         $this->rules = $rules;
-        $this->reasons = [
-            'email' => 'The :attribute format is invalid.',
-            'ip' => 'The :attribute format is invalid.',
-            'url' => 'The :attribute format is invalid.',
-            'min' => 'The :attribute must be at least :min.',
-            'max' => 'The :attribute may not be greater than :max.',
-            'required' => 'The :attribute field is required.',
-            'numeric' => 'The :attribute field must be number.',
-            'integer' => 'The :attribute field must be integer.',
-        ];
-        $this->fire();
+        $this->start();
     }
-    public function fire()
+
+    public function start()
     {
         foreach ($this->rules as $attribute => $rule) {
             foreach (explode('|', $rule) as $item) {
                 $detial = explode(':', $item);
                 $data = isset($this->data[$attribute])?$this->data[$attribute]:null;
-                if (count($detial) > 1) {
-                    $reason = $this->{$detial[0]}($data, $detial[1]);
-                } else {
+                if (count($detial) === 1) {
                     $reason = $this->$item($data);
-                }
-                if ($reason !== true) {
-                    $this->errors[] = str_replace(':attribute', $attribute, $reason);
+                    if ($reason === false) {
+                        $reason = isset($this->reasons[$item])===true? $this->reasons[$item]:'The :attribute format is invalid.';
+                        $this->errors[] = str_replace(':attribute', $attribute, $reason);
+                    }
+                } elseif (count($detial) === 2) {
+                    $reason = $this->{$detial[0]}($data,explode(',', $detial[1]));
+                    if ($reason === false) {
+                        $reason = isset($this->reasons[$detial[0]])===true? $this->reasons[$detial[0]]:'The :attribute format is invalid.';
+                        $this->errors[] = str_replace(':'.$detial[0], '['.$detial[1].']', str_replace(':attribute', $attribute, $reason));
+                    }
+                } else {
+                    throw new \LengthException(" $attribute parameters");
                 }
             }
         }
@@ -55,44 +75,69 @@ class Validator
             return false;
         } elseif (is_string($value) && trim($value) === '') {
             return false;
-        } elseif ((is_array($value) || $value instanceof Countable) && count($value) < 1) {
+        } elseif ((is_array($value) || $value instanceof \Countable) && count($value) < 1) {
             return false;
-        } elseif ($value instanceof File) {
+        } elseif ($value instanceof \File) {
             return (string) $value->getPath() != '';
         }
-
         return true;
-        // return !$value ? $this->reasons['required'] : true;
     }
+
+    /**
+     * required_if:anotherfield,value,…
+
+    * 验证字段在另一个字段等于指定值value时是必须的
+     *
+     * @param [type] $value
+     * @param [type] $parameters
+     * @return void
+     * @author S.Y <yangsheng@yahoo.com>
+     * @version 1.0.0
+     */
+    protected function required_if($value, $parameters)
+    {
+        $this->requireParameterCount(2, $parameters, 'required_if');
+        if($this->data[$parameters[0]]==$parameters[1]){
+            return $this->required($value);
+        }
+        return true;
+    }
+
     protected function email($value)
     {
-        return filter_var($value, FILTER_VALIDATE_EMAIL) ? true : $this->reasons['email'];
-    }
-    protected function min($value, $min)
-    {
-        return mb_strlen($value, 'UTF-8') >= $min ? true : str_replace(':min', $min, $this->reasons['min']);
-    }
-    protected function max($value, $max)
-    {
-        return mb_strlen($value, 'UTF-8') <= $max ? true : str_replace(':max', $max, $this->reasons['max']);
+        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
     }
     protected function numeric($value)
     {
-        return is_numeric($value) ? true : $this->reasons['numeric'];
+        return is_numeric($value);
     }
     protected function integer($value)
     {
-        return filter_var($value, FILTER_VALIDATE_INT) !== false ? true : $this->reasons['integer'];
+        return filter_var($value, FILTER_VALIDATE_INT) !== false;
     }
-    protected function string($value){
+    protected function string($value)
+    {
         return is_string($value);
+    }
+
+    public function array($value)
+    {
+        return is_array($value);
+    }
+
+    public function json($value)
+    {
+        if (! is_scalar($value) && ! method_exists($value, '__toString')) {
+            return false;
+        }
+        json_decode($value);
+        return json_last_error() === JSON_ERROR_NONE;
     }
 
     protected function ip($value)
     {
-        return filter_var($value, FILTER_VALIDATE_IP) !==false?true:$this->reasons['ip'];
+        return filter_var($value, FILTER_VALIDATE_IP) !==false;
     }
-
     public function ipv4($value)
     {
         return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) !== false;
@@ -102,22 +147,18 @@ class Validator
         return filter_var($value, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) !== false;
     }
 
-    public function json($value)
-    {
-        if (! is_scalar($value) && ! method_exists($value, '__toString')) {
-            return false;
-        }
-
-        json_decode($value);
-
-        return json_last_error() === JSON_ERROR_NONE;
-    }
-
     protected function url($value)
     {
-        return filter_var($value, FILTER_VALIDATE_URL) !==false?true:$this->reasons['ip'];
+        return filter_var($value, FILTER_VALIDATE_URL) !==false;
     }
-
+    /**
+     * 验证字段必须是一个基于PHP strtotime函数的有效日期
+     *
+     * @param [type] $value
+     * @return void
+     * @author S.Y <yangsheng@yahoo.com>
+     * @version 1.0.0
+     */
     public function date($value)
     {
         if ($value instanceof \DateTime) {
@@ -125,11 +166,6 @@ class Validator
         }
         $date = date_parse($value);
         return checkdate($date['month'], $date['day'], $date['year']);
-    }
-
-    public function in($value, $range)
-    {
-        var_dump($range);
     }
     /**
      * 字段仅全数为字母字串时通过验证。
@@ -168,26 +204,72 @@ class Validator
         return preg_match('/^[\pL\pM\pN]+$/u', $value) > 0;
     }
 
-    public function array($value)
+    /**
+     * 验证两字段相等
+     *
+     * @param [type] $value
+     * @param [type] $parameters
+     * @return void
+     * @author S.Y <yangsheng@yahoo.com>
+     * @version 1.0.0
+     */
+    public function confirmed($value, $parameters)
     {
-        return is_array($value);
+        $this->requireParameterCount(1, $parameters, 'min');
+        return $value === $this->data[$parameters[0]];
+    }
+   
+    protected function min($value, $parameters)
+    {
+        $this->requireParameterCount(1, $parameters, 'min');
+        return $value >= $parameters === true;
+    }
+    protected function max($value, $parameters)
+    {
+        $this->requireParameterCount(1, $parameters, 'max');
+        return $value <= $parameters === true;
+    }
+
+    protected function length($value, $parameters)
+    {
+        
+        $this->requireParameterCount(2, $parameters, 'length');
+        $length = mb_strlen($value, 'UTF-8');
+        return $length >= $parameters[0] === true && $length <= $parameters[1]===true;
+    }
+   
+
+    public function in($value, $parameters)
+    {
+        $this->requireParameterCount(1, $parameters, 'in');
+        return in_array($value, $parameters) === true;
+    }
+
+    public function not_in($value, $parameters)
+    {
+        $this->requireParameterCount(1, $parameters, 'in');
+        return in_array($value, $parameters)===false;
+    }
+
+    /**
+     * Require a certain number of parameters to be present.
+     *
+     * @param [type] $count
+     * @param [type] $parameters
+     * @param [type] $rule
+     * @return void
+     * @author S.Y <yangsheng@yahoo.com>
+     * @version 1.0.0
+     */
+    protected function requireParameterCount($count, $parameters, $rule)
+    {
+        if (count($parameters) < $count) {
+            throw new \InvalidArgumentException("Validation rule {$rule} requires at least {$count} parameters.");
+        }
     }
 
     public function __call($method, $parameters)
     {
-        throw new \UnexpectedValueException("Validate rule [$method] does not exist!");
+        throw new \UnexpectedValueException("Validate rule [{$method}] does not exist!");
     }
 }
-$data = [
-
-];
-$validator = new Validator($data, [
-    // 'customer_number' => 'required|numeric|integer',
-    'name' => 'required|min:2|max:25',
-    'sex' => 'required|integer|in:[1,2]',
-    // 'id_type' => 'required|integer',
-    // 'id_number' => 'required',
-    // 'phone' => 'required',
-    // 'email' => 'email',
-]);
-var_dump($validator->errors);
